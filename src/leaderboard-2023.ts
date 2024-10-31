34999b146e04ebcae17b1fef15c5d8a07e9c00a2
Matthias Nannt <mail@matthiasnannt.com>
2024-10-31 16:04:55 +0100

    add new leaderboard for 2024

---
+++ src/leaderboard-2023.ts
@@ -0,0 +1,402 @@
+export const leaderboard = [
+  {
+    name: "Piyush",
+    points: "4045",
+    link: "https://github.com/gupta-piyush19",
+  },
+  {
+    name: "Suman",
+    points: "1000",
+  },
+  {
+    name: "Subhdeep",
+    points: "100",
+  },
+  {
+    name: "Pratik",
+    points: "1350",
+  },
+  {
+    name: "Karuppiah",
+    points: "100",
+  },
+  {
+    name: "Arth",
+    points: "100",
+  },
+  {
+    name: "Neztep",
+    points: "100",
+  },
+  {
+    name: "Kelvin Parmar",
+    points: "200",
+  },
+  {
+    name: "Naitik Kapadia (Arjun)",
+    points: "3250",
+    link: "https://github.com/KapadiaNaitik",
+  },
+  {
+    name: "Yashhhh",
+    points: "200",
+  },
+  {
+    name: "vishleshak",
+    points: "100",
+  },
+  {
+    name: "Ashu999",
+    points: "100",
+  },
+  {
+    name: "Sachin H",
+    points: "100",
+  },
+  {
+    name: "Suraj Jadhav",
+    points: "100",
+  },
+  {
+    name: "Vishrut",
+    points: "250",
+  },
+  {
+    name: "cataxcab",
+    points: "100",
+  },
+  {
+    name: "Eldemarkki",
+    points: "600",
+  },
+  {
+    name: "Suyash",
+    points: "100",
+  },
+  {
+    name: "Nafees Nazik",
+    points: "600",
+  },
+  {
+    name: "monk",
+    points: "100",
+  },
+  {
+    name: "Pratik Tiwari (Pratik)",
+    points: "100",
+  },
+  {
+    name: "Adarsh Malviya",
+    points: "450",
+  },
+  {
+    name: "Aditya Deshlahre",
+    points: "2120",
+    link: "https://github.com/adityadeshlahre",
+  },
+  {
+    name: "Rutam",
+    points: "1105",
+  },
+  {
+    name: "Sagnik Sahoo",
+    points: "250",
+  },
+  {
+    name: "Prasoon Mahawar",
+    points: "1600",
+  },
+  {
+    name: "Dushmanta",
+    points: "100",
+  },
+  {
+    name: "Arjavv",
+    points: "100",
+  },
+  {
+    name: "Ashish Khare",
+    points: "100",
+  },
+  {
+    name: "Rohit Mondal",
+    points: "205",
+  },
+  {
+    name: "thanmaisai",
+    points: "2825",
+  },
+  {
+    name: "Rayyan Alam (Rayy)",
+    points: "100",
+  },
+  {
+    name: "Ayush",
+    points: "100",
+  },
+  {
+    name: "Zechariah",
+    points: "100",
+  },
+  {
+    name: "Rajarshi Misra",
+    points: "500",
+  },
+  {
+    name: "Anjaneya Gupta",
+    points: "3150",
+  },
+  {
+    name: "Sachin Kuber",
+    points: "100",
+  },
+  {
+    name: "Manpreet Singh",
+    points: "100",
+  },
+  {
+    name: "Vaibhav Gupta",
+    points: "100",
+  },
+  {
+    name: "maciek",
+    points: "650",
+  },
+  {
+    name: "yatharth",
+    points: "650",
+  },
+  {
+    name: "Ratish Jain",
+    points: "300",
+    link: "https://github.com/ratishjain12",
+  },
+  {
+    name: "Subham Raj",
+    points: "500",
+  },
+  {
+    name: "Abhinav Arya",
+    points: "1100",
+    link: "github.com/itzabhinavarya",
+  },
+  {
+    name: "Yash Nirmal",
+    points: "200",
+  },
+  {
+    name: "Rohan Gupta",
+    points: "1350",
+    link: "https://github.com/rohan9896",
+  },
+  {
+    name: "Eder Silva",
+    points: "100",
+    link: "https://github.com/edersilva78",
+  },
+  {
+    name: "Eduardo Noronha",
+    points: "200",
+  },
+  {
+    name: "Joyal",
+    points: "650",
+    link: "https://github.com/joyal007",
+  },
+  {
+    name: "zaCKoZAck0",
+    points: "200",
+  },
+  {
+    name: "Viswa Prasath (iamvp7)",
+    points: "100",
+  },
+  {
+    name: "aman4444",
+    points: "100",
+  },
+  {
+    name: "mohit.d404",
+    points: "100",
+  },
+  {
+    name: "0xCgn",
+    points: "100",
+  },
+  {
+    name: "bitnagar",
+    points: "150",
+  },
+  {
+    name: "United1l",
+    points: "350",
+  },
+  {
+    name: "Arya Bhosale",
+    points: "300",
+  },
+  {
+    name: "Bhavya",
+    points: "100",
+  },
+  {
+    name: "Bilal Mirza",
+    points: "2095",
+  },
+  {
+    name: "Asharan2511",
+    points: "50",
+  },
+  {
+    name: "bhimanshu",
+    points: "50",
+  },
+  {
+    name: "Digvijay Gupta",
+    points: "300",
+  },
+  {
+    name: "hercules_2401",
+    points: "100",
+  },
+  {
+    name: "iampolska742",
+    points: "50",
+  },
+  {
+    name: "Jonas Hoebenreich",
+    points: "100",
+  },
+  {
+    name: "Markson",
+    points: "100",
+  },
+  {
+    name: "Mohd Zama",
+    points: "50",
+  },
+  {
+    name: "Moheyt",
+    points: "550",
+  },
+  {
+    name: "ortin779",
+    points: "275",
+  },
+  {
+    name: "Piyush Garg",
+    points: "650",
+  },
+  {
+    name: "Sachin Mittal",
+    points: "850",
+  },
+  {
+    name: "Sha1kh4",
+    points: "50",
+  },
+  {
+    name: "Soham Tembhurne",
+    points: "50",
+  },
+  {
+    name: "Sundaram2021",
+    points: "50",
+  },
+  {
+    name: "tarunclub",
+    points: "50",
+  },
+  {
+    name: "thevinitgupta",
+    points: "50",
+  },
+  {
+    name: "Vishal Khoje",
+    points: "100",
+  },
+  {
+    name: "Aadish1233",
+    points: "50",
+  },
+  {
+    name: "aashish",
+    points: "980",
+  },
+  {
+    name: "AliYar-Khan",
+    points: "50",
+  },
+  {
+    name: "Hanan Mehmood",
+    points: "50",
+  },
+  {
+    name: "Janumala Akhilendra",
+    points: "50",
+  },
+  {
+    name: "Nilanjan Pramanik",
+    points: "50",
+  },
+  {
+    name: "Vedant-Z",
+    points: "200",
+  },
+  {
+    name: "Vineetjk",
+    points: "50",
+  },
+  {
+    name: "Hemanth Wasthere",
+    points: "100",
+  },
+  {
+    name: "Ola",
+    points: "1800",
+  },
+  {
+    name: "Olalaye Blessing",
+    points: "150",
+  },
+  {
+    name: "Raju Gangitla",
+    points: "100",
+  },
+  {
+    name: "Ronit Panda",
+    points: "1650",
+  },
+  {
+    name: "Shyam Raghu",
+    points: "500",
+  },
+  {
+    name: "Vikas Patil",
+    points: "250",
+  },
+  {
+    name: "0Armaan025",
+    points: "100",
+  },
+  {
+    name: "mandharet",
+    points: "100",
+  },
+  {
+    name: "Harshit Vashisht",
+    points: "200",
+  },
+  {
+    name: "JiyaGupta-cs",
+    points: "50",
+  },
+  {
+    name: "Kurayami",
+    points: "100",
+  },
+  {
+    name: "Sandy-1711",
+    points: "50",
+  },
+];
